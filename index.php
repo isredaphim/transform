b74eab34976820d47dc996a682e762f506856a21
isredaphim <55270578+isredaphim@users.noreply.github.com>
2020-09-05 21:30:39 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+
+<html>
+<head>
+<title>Wireframe</title>
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="toppings">
+
+		<div class="Logoo"> 
+			<div id="triangle-up"></div>
+			<div id="triangle-topleft"></div>
+			<div id="triangle-topright"></div>
+			<div id="triangle-bottomleft"></div>
+			<div id="triangle-bottomright"></div>
+		
+		
+		
+		
+		
+		
+		</div>
+		
+		
+		
+		
+		
+		
+		
+		<div class="Navtop">
+			<div class="Bars"></div>
+			<div class="Bars"></div>
+			<div class="Bars"></div>
+
+		</div>
+
+	</div>
+
+<div class="bodding">
+	<div class="navside">
+
+		<div class="navigates"> Navigations</div>
+		<div class="navigates"> Navigations</div>
+		<div class="navigates"> Navigations</div>
+		<div class="navigates"> Navigations</div>
+		<div class="navigates"> Navigations</div>
+		<div class="navigates"> Navigations</div>
+		<div class="navigates"> Navigations</div>
+
+	</div>
+
+<div class="contents"> 
+		<div class="contented"> <img src="pics/background.jpg" style="width: 850px; height: 400px;"/> </div>
+		<div class="botcont"> 
+				<div class="boxes"> <img src="pics/pic2.jpg" style="width: 250px; height: 200px;"/> </div>
+				<div class="boxes"> <img src="pics/pic3.jpg" style="width: 250px; height: 200px;"/> </div>
+				<div class="boxes"> <img src="pics/pic4.jpg" style="width: 250px; height: 200px;"/> </div>
+		</div>
+
+
+</div>
+</div>
+
+
+
+
+</body>
+</html>
